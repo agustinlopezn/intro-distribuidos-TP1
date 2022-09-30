c6fdd2f170bdf8f38880411cad4c19b3e4974220
Andy <anfernandez@fi.uba.ar>
2022-09-30 16:55:16 -0300

    socket type determined on environment variable

--- src/lib/accepter.py
+++ src/lib/accepter.py
@@ -1,10 +1,14 @@
 from src.lib.custom_socket.gbn_socket import GBNSocket
 from src.lib.custom_socket.saw_socket import SaWSocket
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 class Accepter:
     def __init__(self, **kwargs):
-        self.socket = GBNSocket(opposite_address=None, **kwargs)
+        socket_type = SaWSocket if os.getenv("SOCKET_TYPE") == "SaW" else GBNSocket
+        self.socket = socket_type(opposite_address=None, **kwargs)
         self.socket.set_timeout(None)
 
     def accept(self):
--- src/lib/file_handler/file_handler.py
+++ src/lib/file_handler/file_handler.py
@@ -2,20 +2,18 @@ import threading
 from src.lib.custom_socket.gbn_socket import GBNSocket
 from src.lib.custom_socket.saw_socket import SaWSocket
 from src.lib.logger import Logger
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 class FileHandler:
     CHUNK_SIZE = 65536
 
-    def __init__(self, opposite_address, logger, host="", port=0):
-        super().__init__()
-        self.socket = GBNSocket(
-            opposite_address=opposite_address,
-            host=host,
-            port=port,
-            logger=logger,
-        )
-        self.opposite_address = opposite_address
+    def __init__(self, logger, **kwargs):
+        socket_type = SaWSocket if os.getenv("SOCKET_TYPE") == "SaW" else GBNSocket
+        self.socket = socket_type(logger=logger, **kwargs)
         self.logger = logger
         self.file_name = None  # not necessary, just works as variable declaration
         self.file_size = None  # not necessary, just works as variable declaration
